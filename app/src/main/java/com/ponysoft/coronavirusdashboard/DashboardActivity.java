8c62b98c6ecf2a84e922b7d2d9f5d4c5f7837eeb
codefrog <63029867+codefrog55789@users.noreply.github.com>
2020-04-08 14:03:35 +0800

    develop: add viewpager, add fragments

--- app/src/main/java/com/ponysoft/coronavirusdashboard/DashboardActivity.java
+++ app/src/main/java/com/ponysoft/coronavirusdashboard/DashboardActivity.java
@@ -1,29 +1,66 @@
 package com.ponysoft.coronavirusdashboard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import com.ponysoft.api.DataAPI;
-import com.ponysoft.models.QuickAllModel;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.ponysoft.fragments.dashboard.ConDashboardFragment;
+import com.ponysoft.fragments.dashboard.QuickFactsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private List<Fragment> fragmentArrayList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DataAPI.shareInstance().getQuickAll(new DataAPI.OnQuickAllListener() {
-            @Override
-            public void success(int code, QuickAllModel model) {
+        // Initial Fragments
 
-                Log.v("MainActivity", String.valueOf(model.updated));
-            }
+        fragmentArrayList = new ArrayList<>();
+        fragmentArrayList.add(new QuickFactsFragment());
+        fragmentArrayList.add(new ConDashboardFragment());
 
-            @Override
-            public void fail(int code, String message) {
-            }
-        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        DashboardFragmentPageAdapter dashboardFragmentPageAdapter = new DashboardFragmentPageAdapter(fragmentManager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        dashboardFragmentPageAdapter.setFragmentList(fragmentArrayList);
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.dashboard_view_pager);
+        viewPager.setAdapter(dashboardFragmentPageAdapter);
+    }
+
+    class DashboardFragmentPageAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragmentList = null;
+
+        public DashboardFragmentPageAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        public void setFragmentList(List<Fragment> list) {
+
+            fragmentList = list;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
     }
 }
